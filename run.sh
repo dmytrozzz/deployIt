f6354b45dc9748f8ae679ed323ee64991fbf20e9
dmytrozzz <d.khaynas@gmail.com>
2016-03-22 14:43:25 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#dropbear -REmwg -p 22
+function lazyclone {
+    url=$1;
+    reponame=$(echo $url | awk -F/ '{print $NF}' | sed -e 's/.git$//');
+    git clone $url $reponame;
+    cd $reponame;
+}
+lazyclone $REPO
+./gradlew build
+nohup ./gradlew bootRun &
+while true
+do
+ echo "Git pull, yo!"
+ git pull | grep -v "up-to-date" && ./gradlew build
+ sleep 5
+done
+#tail -f /dev/null
